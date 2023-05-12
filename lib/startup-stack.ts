700827b2288485a8611e19e72b61989e721dd51c
Daniel Oraca <danieloraca@daniels-mbp.home>
2023-05-12 21:06:00 +0100

    add resources to stack

--- lib/startup-stack.ts
+++ lib/startup-stack.ts
@@ -1,16 +1,75 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as nodejslambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class StartupStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //dynamo table
+    const table = new dynamodb.Table(this, 'DynamoDan', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'DynamoDanTable'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StartupQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    const getLambda = new nodejslambda.NodejsFunction(this, "GetLambda", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      environment: {
+        'TABLE_NAME': table.tableName
+      },
+      handler: "index.handler",
+      entry: "from_lambda/index.js",
+      bundling: {
+        externalModules: [
+          'aws-sdk'
+        ]
+      }
+    });
+
+    // const quoteLambda = new nodejslambda.NodejsFunction(this, "QuoteLambda", {
+    //   runtime: lambda.Runtime.NODEJS_16_X,
+    //   environment:  {
+    //     'TABLE_NAME': table.tableName
+    //     },
+    //   handler: "index.handler",
+    //   entry: "from_lambda/index.js",
+    //   bundling: {
+    //     externalModules: [
+    //       'aws-sdk'
+    //     ]
+    //   }
     // });
+
+    // table.grantReadData(quoteLambda);
+
+    // const putLambda = new nodejslambda.NodejsFunction(this, "PutLambda", {
+    //   runtime: lambda.Runtime.NODEJS_16_X,
+    //   environment:  {
+    //     'TABLE_NAME': table.tableName
+    //     },
+    //     handler: "index.handler",
+    //     entry: "from_lambda/index.js",
+    //     bundling: {
+    //       externalModules: [
+    //         'aws-sdk'
+    //       ]
+    //     }
+    // });
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, "Api");
+
+    api.root.addMethod("GET", new apigateway.LambdaIntegration(getLambda));
+    // api.root.addMethod("POST", new apigateway.LambdaIntegration(putLambda));
+    
+    new cdk.CfnOutput(this, "Endpoint", {
+      value: api.url!,
+      description: "The API endpoint",
+    });
+
   }
 }
