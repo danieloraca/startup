1c95243c15d14d206e325af86f679f59a78dfe96
Daniel Oraca <danieloraca@users.noreply.github.com>
2023-05-12 14:35:02 +0100

    Initial commit

---
+++ test/startup.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Startup from '../lib/startup-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/startup-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Startup.StartupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
